a049e7b4d2df8b4869b16bb4df7def26a7d94c43
JulioTraco <jftraconiz@gmail.com>
2023-12-06 18:24:31 -0500

    Agregue una showbox  en el form1

--- Ordinario/Form1.cs
+++ Ordinario/Form1.cs
@@ -6,5 +6,10 @@ namespace Ordinario
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rubem is ass ");
+        }
     }
 }
