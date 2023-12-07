a8f79c1e659127c0fa00cab44954d437d6527655
SalazarDaniel <danielsalazarberzunza@hotmail.com>
2023-12-06 19:20:11 -0600

    Añadi un boton

--- Ordinario/Form1.cs
+++ Ordinario/Form1.cs
@@ -11,5 +11,10 @@ namespace Ordinario
         {
             MessageBox.Show("Alan es un puto perro pendejo de mierda");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ruben es un buen chico");
+        }
     }
 }
